ec48bde450c915e390077fad9c76642919ae93ef
Samandar0912 <samandarnazarov07@gmail.com>
2025-04-19 18:09:13 +0500

    v3

--- core/settings.py
+++ core/settings.py
@@ -89,17 +89,17 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Internationalization
+LANGUAGES = [
+    ('uz', 'O‘zbekcha'),
+    ('en', 'English'),
+    ('ru', 'Русский'),
+]
+
 LANGUAGE_CODE = 'uz'
+
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
-LANGUAGES = [
-    ('uz', _('Uzbek')),
-    ('en', _('English')),
-    ('ru', _('Russian')),
-]
 
 LOCALE_PATHS = [
     BASE_DIR / 'locale',
